5ca5f8cd8742191fd724140271f3c26ccc7eeb6d
Laura Collins <laura.e.collins1@gmail.com>
2019-09-28 10:38:18 -0500

    Done with condiments

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,13 @@ import bread from './components/bread.js';
 import meat from './components/meat.js';
 import order from './components/order.js';
 import cheese from './components/cheese.js';
+import condiments from './components/condiments.js';
 
 const init = () => {
     bread.printBreadOptions();
     meat.printMeatOptions();
     cheese.printCheeseOptions();
+    condiments.printCondimentOptions();
     order.printOrderButton();
 };
 
